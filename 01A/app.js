af0bf443ab981de5c24b2edc98c98e04e69ff3ae
mauser051 <145978301+mauser051@users.noreply.github.com>
2023-11-21 15:06:27 +0100

    2

--- 01A/app.js
+++ 01A/app.js
@@ -14,10 +14,14 @@ let appName = "demoapp"
 let versie = 0.1
 let versiedatum = "14:45 21/11/2023 (central eupean time)"
 let autheur = "leraar"
+let copyright = "leraar BV"
+let distributeur = "leraar soft"
 let darkmode = false
 
 console.log("appname: " + appName);
 console.log("versie: " + versie);
 console.log("versiedatum: " + versiedatum);
 console.log("autheur: " + autheur);
+console.log("copyright: " + copyright)
+console.log("distributeur" + distributeur)
 console.log("darkmode: " + darkmode)
