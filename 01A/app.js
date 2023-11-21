8ea4b0106a370ae02e6a5ca362f4e8915c6014cb
mauser051 <145978301+mauser051@users.noreply.github.com>
2023-11-21 14:59:15 +0100

    1

---
+++ 01A/app.js
@@ -0,0 +1,23 @@
+class App
+{
+    runAplication()
+    {
+        console.log("hello world");
+    }
+}
+
+let app = new App();
+app.runAplication();
+
+
+let appName = "demoapp"
+let versie = 0.1
+let versiedatum = "14:45 21/11/2023 (central eupean time)"
+let autheur = "leraar"
+let darkmode = false
+
+console.log("appname: " + appName);
+console.log("versie: " + versie);
+console.log("versiedatum: " + versiedatum);
+console.log("autheur: " + autheur);
+console.log("darkmode: " + darkmode)
